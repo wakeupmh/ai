c411b664edeea3f712c919e8e117e2201d54b2ce
marcos.henrique <wakeupmh@gmail.com>
2024-05-01 18:24:24 -0300

    feat: add post note

--- nextjs-ai-notes/src/components/ui/AddNoteDialog.tsx
+++ nextjs-ai-notes/src/components/ui/AddNoteDialog.tsx
@@ -1,23 +1,49 @@
 import { CreateNoteSchema, createNoteSchema } from "@/lib/validation/note";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./dialog";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "./dialog";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./form";
+import { Input } from "./input";
+import { Textarea } from "./textarea";
+import LoadingButton from "./loading-button";
+import { useRouter } from "next/navigation";
 
 interface AddNoteDialogProps {
   open: boolean;
   setOpen: (open: boolean) => void;
 }
 
-export default function AddNoteDialog({
-  open,
-  setOpen
-}: AddNoteDialogProps) {
+export default function AddNoteDialog({ open, setOpen }: AddNoteDialogProps) {
+  const router = useRouter();
   const form = useForm<CreateNoteSchema>({
     resolver: zodResolver(createNoteSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+    }
   });
 
   async function onSubmit(input: CreateNoteSchema) {
-    console.log(input);
+    try{
+      const response = await fetch("/api/notes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(input),
+      });
+
+      if(!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      form.reset();
+      router.refresh(); // Refresh the page to show the new note as it's server component
+      setOpen(false);
+    } catch (error) {
+      console.error("Failed to add note", error);
+      alert("Failed to add note, please try again.")
+    }
   }
 
   return (
@@ -26,7 +52,42 @@ export default function AddNoteDialog({
         <DialogHeader>
           <DialogTitle>Add Note</DialogTitle>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Note title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Note title" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="content"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Note content</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Note content" {...field} value={field.value ?? ''} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <LoadingButton type="submit" loading={form.formState.isSubmitting}>
+                Submit
+              </LoadingButton>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
